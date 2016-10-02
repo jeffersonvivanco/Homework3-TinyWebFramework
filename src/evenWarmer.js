4124a42a674c6d7eb0d4efdb55a8f8c5a14a4259
jeffersonvivanco <jv1019@nyu.edu>
2016-10-02 12:52:18 -0400

    First Commit

--- src/evenWarmer.js
+++ src/evenWarmer.js
@@ -14,18 +14,36 @@ var functions = {
         var requestLine = requestParts[0].split(' ');
         var method = requestLine[0];
         var path = requestLine[1];
-        var header1 = requestParts[1].split(' ');
-        var header2 = requestParts[2].split(' ');
+        var host = requestParts[1].split(' ');
+        var referer = requestParts[2].split(' ');
         var body = requestParts[3];
 
         this.method = method;
         this.path = path;
-        this.headers = {
-            Host: header1[1],
-            Referer : header2[1]
+        if(host[1] && referer[1]){
+            this.headers = {
+                Host: host[1],
+                Referer : referer[1]
+            }
         }
+        else{
+            this.headers = {
+                Host:host[1]
+            }
+        }
+        this.body = body;
+        this.toString = function () {
+            return s;
+        };
+
 
 }
 };
-
+var s = '';
+s += 'GET /foo.html HTTP/1.1\r\n';   // request line
+s += 'Host: localhost:8080\r\n';     // headers
+s += 'Referer: http://bar.baz/qux.html\r\n';
+s += '\r\n';
+var req = new functions.Request(s);
+console.log(req);
 module.exports = functions;
